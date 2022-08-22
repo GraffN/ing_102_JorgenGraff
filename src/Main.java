48f6936938f6257828c15536b12226f186dbb91d
GraffN <jorgen.graff-n@outlook.com>
2022-08-22 13:10:39 +0200

    First commit to organization

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        for(int i = 0; i < 5; i++){
+            System.out.print("*");
+            for(int k = 0; k<5; k++){
+                System.out.print("*");
+            }
+            System.out.println(",");
+        }
+    }
+}
